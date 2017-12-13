6e301827aebdaf7af2cbb176820ffdc3b78dc8ec
Satyen Rai <satyenr@users.noreply.github.com>
2017-12-13 20:43:50 +0530

    hs/2017/13: check in partial solution
    It is incomplete and broken.

---
+++ 2017/day13.hs
@@ -0,0 +1,24 @@
+import qualified Data.Map as M
+
+main :: IO ()
+main = do
+--     input <- getContents
+    let scnData = M.fromList [(0, 3), (1, 2), (4, 4), (6, 4)]
+    print (totalPenalty scnData)
+
+scannerPosition :: Int -> Int -> Int
+scannerPosition psec range
+    | 0 <- range        = -1
+    | odd quotient  = limit - (psec `mod` limit)
+    | even quotient = psec `mod` limit
+    where quotient = psec `div` limit
+          limit    = range - 1
+
+totalPenalty :: M.Map Int Int -> [Int]
+totalPenalty scnData = take 10 [ penalty (depth psec) | psec <- [0 .. ], scannerPosition psec (range $ depth psec) == depth psec]
+    where depth ps = ps
+          range dep = case lookup dep scnData of
+                          Nothing -> 0
+                          Just n  -> n
+          penalty dep = dep * (range dep)
+
