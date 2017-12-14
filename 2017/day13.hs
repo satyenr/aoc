f589ee6a4421a1ea2f39da3d0639525c043234cd
Satyen Rai <satyenr@users.noreply.github.com>
2017-12-14 19:22:28 +0530

    hs/2017/13: check in another partial solution
    Works except reading input from stdin

--- 2017/day13.hs
+++ 2017/day13.hs
@@ -1,22 +1,28 @@
+import           Data.Map (Map)
 import qualified Data.Map as M
 
 main :: IO ()
 main = do
---     input <- getContents
-    let scnData = M.fromList [(0, 3), (1, 2), (4, 4), (6, 4)]
+     input <- getContents
+--    let scnData = M.fromList [(0, 3), (1, 2), (4, 4), (6, 4)]
     print (totalPenalty scnData)
 
-scannerPosition :: Int -> Int -> Int
-scannerPosition psec range
-    | 0 <- range        = -1
-    | odd quotient  = limit - (psec `mod` limit)
+transform :: String -> Map Int Int
+transform input = [map (read::String->Int) x | x <- (map words $ lines input)]
+
+scnPos :: Int -> Int -> Int
+scnPos psec range
+    | 0 <- range    = -1
+    | 1 <- range    = 0
+    | odd  quotient = limit - (psec `mod` limit)
     | even quotient = psec `mod` limit
     where quotient = psec `div` limit
           limit    = range - 1
 
-totalPenalty :: M.Map Int Int -> [Int]
-totalPenalty scnData = take 10 [ penalty (depth psec) | psec <- [0 .. 10], scannerPosition psec (range $ depth psec) == depth psec]
-    where depth ps    = ps
+
+totalPenalty :: Map Int Int -> Int
+totalPenalty scnData = sum [penalty (pktPos psec) | psec <- [0..up], ((scnPos psec (range $ pktPos psec)) == 0)]
+    where pktPos ps   = ps
           range dep   = M.findWithDefault 0 dep scnData
           penalty dep = dep * (range dep)
-
+          up          = maximum $ M.keys scnData
