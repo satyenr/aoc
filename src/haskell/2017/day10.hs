9999d0cb2525e1f77ccba22aef51e6ad722d6885
Satyen Rai <satyenr@users.noreply.github.com>
2017-12-28 04:13:16 +0530

    hs/2017/10: attempt to fix the initial solution
    Still broken, unfortunately.

--- src/haskell/2017/day10.hs
+++ src/haskell/2017/day10.hs
@@ -4,12 +4,22 @@ main :: IO ()
 main = do
     input <- getContents
     let lengths = map (read::String->Int) $ splitOn "," input
-    print (product $ take 2 $ twist [0 .. 255] lengths 0 0)
+    print (product $ take 2 $ knot [0 .. 255] lengths 0 0)
 
-twist :: [Int] -> [Int] -> Int -> Int -> [Int]
-twist xs [] _ _ = xs
-twist xs len cur skip = twist xs' len' cur' (skip + 1)
-    where xs'    = take (length xs) $ (reverse $ take curlen $ drop cur $ cycle xs) ++ (drop (curlen + cur) $ cycle xs)
-          len'   = tail len
-          curlen = head len
-          cur'   = cur + skip + curlen
+-- twist :: [Int] -> [Int] -> Int -> Int -> [Int]
+-- twist xs [] _ _ = xs
+-- twist xs len cur skip = twist xs' len' cur' (skip + 1)
+--     where xs'    = take (length xs) $ (reverse $ take curlen $ drop cur $ cycle xs) ++ (drop (curlen + cur) $ cycle xs)
+--           len'   = tail len
+--           curlen = head len
+--           cur'   = cur + skip + curlen
+
+knot :: [Int] -> [Int] -> Int -> Int -> [Int]
+knot xs [] _ _ = xs
+knot xs len cur skip = knot xs' len' cur' (skip + 1)
+          where xs'         = rotate ((length xs) - cur) (reverse . fst $ splitRot xs) ++ (snd $ splitRot xs)
+                clen        = head len
+                rotate n ys = take (length ys) $ drop n (cycle ys)
+                splitRot ys = splitAt clen $ rotate cur ys
+                len'        = tail len
+                cur'        = (cur + skip + clen) `mod` (length xs)
