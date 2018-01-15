b0da4d6536792831571576eac1787f6ab453b6d1
Satyen Rai <satyenr@users.noreply.github.com>
2018-01-15 14:07:28 +0530

    hs/2017/10: refactor code

--- src/haskell/2017/day10.hs
+++ src/haskell/2017/day10.hs
@@ -1,19 +1,21 @@
-import Data.List.Split (splitOn)
 import Control.Applicative
+import Data.Char
+import Data.List.Split
+
 
 main :: IO ()
 main = do
     input <- getContents
-    let lengths = map (read :: String -> Int) $ splitOn "," input
-    print (product $ take 2 $ partOne [0 .. 255] lengths 0 0)
+    let list = [0 .. 255]
 
-partOne :: [Int] -> [Int] -> Int -> Int -> [Int]
-partOne xs [] _ _ = xs
-partOne xs len cur skip = partOne xs' len' cur' skip'
-    where xs'         = rotate (length xs - cur) $ nxt $ rotate cur xs
-          nxt         = liftA2 (++) (reverse . (take curlen)) (drop curlen)
-          len'        = tail len
-          cur'        = (cur + curlen + skip) `mod` (length xs)
-          skip'       = skip + 1
+    -- Part One
+    let lengths = map (read :: String -> Int) $ splitOn "," input
+    print (product $ take 2 $ knotHash lengths list 0 0)
+
+knotHash :: [Int] -> [Int] -> Int -> Int -> [Int]
+knotHash [] list _ _          = list
+knotHash (x:xs) list cur skip = knotHash xs list' cur' (skip + 1)
+    where list'       = rotate (length list - cur) $ nxt $ rotate cur list
+          nxt         = liftA2 (++) (reverse . (take x)) (drop x)
+          cur'        = (cur + x + skip) `mod` (length list)
           rotate n ys = take (length ys) $ drop n $ cycle ys
-          curlen      = head len
