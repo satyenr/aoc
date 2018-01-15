119e6205dbb2511caf3b40d344ac28409812eae5
Satyen Rai <satyenr@users.noreply.github.com>
2018-01-15 11:56:23 +0530

    hs/2017/10: check in working solution
    Finally!

--- src/haskell/2017/day10.hs
+++ src/haskell/2017/day10.hs
@@ -3,23 +3,16 @@ import Data.List.Split (splitOn)
 main :: IO ()
 main = do
     input <- getContents
-    let lengths = map (read::String->Int) $ splitOn "," input
-    print (product $ take 2 $ knot [0 .. 255] lengths 0 0)
+    let lengths = map (read :: String -> Int) $ splitOn "," input
+    print (product $ take 2 $ partOne [0 .. 255] lengths 0 0)
 
--- twist :: [Int] -> [Int] -> Int -> Int -> [Int]
--- twist xs [] _ _ = xs
--- twist xs len cur skip = twist xs' len' cur' (skip + 1)
---     where xs'    = take (length xs) $ (reverse $ take curlen $ drop cur $ cycle xs) ++ (drop (curlen + cur) $ cycle xs)
---           len'   = tail len
---           curlen = head len
---           cur'   = cur + skip + curlen
-
-knot :: [Int] -> [Int] -> Int -> Int -> [Int]
-knot xs [] _ _ = xs
-knot xs len cur skip = knot xs' len' cur' (skip + 1)
-          where xs'         = rotate ((length xs) - cur) (reverse . fst $ splitRot xs) ++ (snd $ splitRot xs)
-                clen        = head len
-                rotate n ys = take (length ys) $ drop n (cycle ys)
-                splitRot ys = splitAt clen $ rotate cur ys
-                len'        = tail len
-                cur'        = (cur + skip + clen) `mod` (length xs)
+partOne :: [Int] -> [Int] -> Int -> Int -> [Int]
+partOne xs [] _ _ = xs
+partOne xs len cur skip = partOne xs' len' cur' skip'
+    where xs'         = rotate (lxs - cur) (reverse (take curlen (rotate cur xs)) ++ (drop curlen (rotate cur xs)))
+          len'        = tail len
+          cur'        = (cur + curlen + skip) `mod` lxs
+          skip'       = skip + 1
+          rotate n ys = take (length ys) $ drop n $ cycle ys
+          curlen      = head len
+          lxs         = length xs
