a7f79b8a16c434bba8d53f230345b8a3b96afd4a
Satyen Rai <satyenr@users.noreply.github.com>
2018-01-15 17:02:14 +0530

    hs/2017/10: check in solution to part 2

--- src/haskell/2017/day10.hs
+++ src/haskell/2017/day10.hs
@@ -1,6 +1,9 @@
-import Control.Applicative
-import Data.Char
-import Data.List.Split
+import Control.Applicative  (liftA2)
+import Data.Bits            (xor)
+import Data.Char            (ord)
+import Data.List.Split      (splitOn, chunksOf)
+import Data.String.Utils    (strip)
+import Numeric              (showHex)
 
 
 main :: IO ()
@@ -10,7 +13,17 @@ main = do
 
     -- Part One
     let lengths = map (read :: String -> Int) $ splitOn "," input
+    print (knotHash lengths list 0 0)
     print (product $ take 2 $ knotHash lengths list 0 0)
+
+    -- Part Two
+    let salt = [17, 31, 73, 47, 23]
+    let bytes = map ord (strip input) ++ salt
+    let shRuns = 64
+    let sh = sparseHash bytes list shRuns
+    let dhChunkSz = 16
+    let dh = denseHash sh dhChunkSz
+    putStrLn (concat $ map hex dh)
 
 knotHash :: [Int] -> [Int] -> Int -> Int -> [Int]
 knotHash [] list _ _          = list
@@ -19,3 +32,16 @@ knotHash (x:xs) list cur skip = knotHash xs list' cur' (skip + 1)
           nxt         = liftA2 (++) (reverse . (take x)) (drop x)
           cur'        = (cur + x + skip) `mod` (length list)
           rotate n ys = take (length ys) $ drop n $ cycle ys
+
+sparseHash :: [Int] -> [Int] -> Int -> [Int]
+sparseHash xs list count =
+    let bytes = take (count * (length xs)) $ cycle xs
+    in knotHash bytes list 0 0
+
+denseHash :: [Int] -> Int -> [Int]
+denseHash xs chunkSz = map (foldl xor 0) $ chunksOf chunkSz xs
+
+hex :: Int -> String
+hex num =
+    let x = showHex num ""
+    in if length x == 1 then "0" ++ x else x
