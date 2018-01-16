0458274edd0bb1f6476d9a8ef57857c5855ddacf
Satyen Rai <satyenr@users.noreply.github.com>
2018-01-16 12:51:31 +0530

    hs/2017/05: check in solution to part 2
    This solution is seriously slow.

--- src/haskell/2017/day05.hs
+++ src/haskell/2017/day05.hs
@@ -2,16 +2,19 @@ main :: IO ()
 main = do
     input <- getContents
     let offsets = map (read :: String -> Int) $ lines input
-    print (steps offsets 0 0)
+    print (steps offsets (+ 1) 0 0)
+    print (steps offsets (\x -> if x >= 3 then x - 1 else x + 1) 0 0)
 
-steps :: [Int] -> Int -> Int -> Int
-steps offsets cur acc
+-- Slow version using immutable data structures
+-- Takes forever for any non-trivial input
+steps :: [Int] -> (Int -> Int) -> Int -> Int -> Int
+steps offsets fn cur acc
     | cur >= length offsets = acc
     | cur < 0               = acc
-    | otherwise             = steps (incr cur offsets) nxt (acc + 1)
-    where rotate 0 xs    = xs
-          rotate n xs    = take (length xs) $ drop n $ cycle xs
-          incr 0 (x:xs)  = (x + 1):xs
-          incr n xs      = rotate (length xs - n) $ (incr 0) $ rotate n xs
-          nxt            = cur + (head . (rotate cur)) offsets
-
+    | otherwise             = steps (mut cur fn offsets) fn nxt (acc + 1)
+    where nxt            = cur + (head . (rotate cur)) offsets
+          mut _ _ []     = []
+          mut 0 f (x:xs) = (f x):xs
+          mut n f xs     = rotate (length xs - n) $ (mut 0 f) $ rotate n xs
+          rotate 0 ys    = ys
+          rotate n ys    = take (length ys) $ drop n $ cycle ys
