29f299364e94db3e542f5fc7b008f4b821437eae
Satyen Rai <satyenr@users.noreply.github.com>
2018-01-16 12:58:19 +0530

    hs/2017/05: add some comments

--- src/haskell/2017/day05.hs
+++ src/haskell/2017/day05.hs
@@ -2,11 +2,15 @@ main :: IO ()
 main = do
     input <- getContents
     let offsets = map (read :: String -> Int) $ lines input
+
+    -- Part 1
     print (steps offsets (+ 1) 0 0)
+
+    -- Part 2
     print (steps offsets (\x -> if x >= 3 then x - 1 else x + 1) 0 0)
 
 -- Slow version using immutable data structures
--- Takes forever for any non-trivial input
+-- Takes forever for any non-trivial input - especially the second part
 steps :: [Int] -> (Int -> Int) -> Int -> Int -> Int
 steps offsets fn cur acc
     | cur >= length offsets = acc
