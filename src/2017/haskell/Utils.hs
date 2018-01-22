e2e8b3e7fd146f695d2cd9164830bb34e088399e
Satyen Rai <satyenr@users.noreply.github.com>
2018-01-23 02:43:30 +0530

    hs: check in Utils module
    It contains commonly used functions

---
+++ src/2017/haskell/Utils.hs
@@ -0,0 +1,8 @@
+module Utils (rotate) where
+
+rotate :: Int -> [a] -> [a]
+rotate n xs 
+    | n == 0    = xs
+    | n == len  = xs
+    | otherwise = take len $ drop n $ cycle xs
+    where len = length xs
