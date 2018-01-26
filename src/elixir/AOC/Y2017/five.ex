94cfc34b5a806cbfae361cf8614f1516d2c9c546
Satyen Rai <satyenr@users.noreply.github.com>
2018-01-26 22:17:13 +0530

    ex/2017/05: check in an efficient version
    It uses mutable erlang arrays since elixir doesn't have an equivalent.

--- src/elixir/AOC/Y2017/five.ex
+++ src/elixir/AOC/Y2017/five.ex
@@ -4,19 +4,20 @@ defmodule AOC.Y2017.Five do
       input
       |> String.split("\n")
       |> Enum.map(&String.to_integer/1)
+      |> :array.from_list
 
     %{ 1 => steps(offsets, &(&1 + 1), 0, 0),
        2 => steps(offsets, &(if &1 < 3 do &1 + 1 else &1 - 1 end), 0, 0) }
   end
 
   defp steps(offsets, mut, cur, acc) do
-    len = length(offsets)
+    len = :array.size(offsets)
     cond do
       cur >= len -> acc
       cur < 0    -> acc
       true       -> 
-        updated_offsets = List.update_at(offsets, cur, mut)
-        jump = cur + elem(List.pop_at(offsets, cur), 0)
+        updated_offsets = :array.set(cur, mut.(:array.get(cur, offsets)), offsets)
+        jump = cur + :array.get(cur, offsets)
         steps(updated_offsets, mut, jump, acc + 1)
     end
   end
