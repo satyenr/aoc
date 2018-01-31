ae0d09f27a303649aec7c1718af49759f6ef3a80
Satyen Rai <satyenr@users.noreply.github.com>
2018-02-01 00:31:01 +0530

    kt/2017/01: use expression syntax for sumDups()

--- src/kotlin/aoc/seventeen/one.kt
+++ src/kotlin/aoc/seventeen/one.kt
@@ -1,13 +1,12 @@
 package aoc.seventeen
 
-private fun sumDups(distance: Int, capcha: String): Int {
-    return capcha.plus(capcha.substring(0, distance))
-            .splitToSequence("")
-            .windowed(distance + 1)
-            .filter { it.last() == it.first() }
-            .map { it.first().toString().toInt() }
-            .sum()
-}
+private fun sumDups(distance: Int, capcha: String) =
+        capcha.plus(capcha.substring(0, distance))
+                .splitToSequence("")
+                .windowed(distance + 1)
+                .filter { it.last() == it.first() }
+                .map { it.first().toString().toInt() }
+                .sum()
 
 fun one(input: String): Pair<String, String> {
     val one = sumDups(1, input).toString()
