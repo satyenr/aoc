2f8d68a7c478646d8e0b0ea467b84ddd78fa5b45
Satyen Rai <satyenr@users.noreply.github.com>
2018-02-02 11:17:49 +0530

    kt/2017/02: replace var by val
    Functions are not mutable.

--- src/kotlin/aoc/seventeen/two.kt
+++ src/kotlin/aoc/seventeen/two.kt
@@ -6,13 +6,13 @@ fun two(input: List<String>): Pair<String, String> {
     val spreadsheet = input.map { it.split("\\s+".toRegex()) }
             .map { row -> row.map { cell -> cell.toInt() }}
 
-    var one = fun(row: List<Int>): Int {
+    val one = fun(row: List<Int>): Int {
         val max = row.max() ?: 0
         val min = row.min() ?: 0
         return max - min
     }
 
-    var two = fun(row: List<Int>): Int {
+    val two = fun(row: List<Int>): Int {
         val sorted = row.sorted()
         return sorted.mapIndexedNotNull({ index, value ->
             val remaining = sorted.drop(index + 1)
