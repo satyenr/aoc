d9a785f5a384fe37ef55620c3f8f13b93d6828eb
Satyen Rai <satyenr@users.noreply.github.com>
2018-02-03 16:12:11 +0530

    Improve Inverse Captcha (Racket)
    Year: 2017
    Day: 1
    Language: Racket

--- src/racket/aoc.rkt
+++ src/racket/aoc.rkt
@@ -7,14 +7,11 @@
   (drop (append lst (take lst n)) n))
 
 (define (sum-dups capcha distance)
-  (let ([fst (lambda (x) (car x))]
-        [snd (lambda (x) (car (cdr x)))]
-        [capcha (filter (lambda (x) (number? x))
-                        (map string->number (string-split capcha "")))])
-    (foldl (lambda (x acc) (+ acc (fst x)))
-           0
-           (filter (lambda (x) (equal? (fst x) (snd x)))
-                   (zip capcha (rotate capcha distance))))))
+  (let ([pair-equal? (lambda (x) (equal? (car x) (car (cdr x))))]
+        [capcha (filter number? (map string->number (string-split capcha "")))])
+    (foldl (lambda (x acc) (+ acc (car x))) 0
+           (filter pair-equal? (zip capcha (rotate capcha distance))))))
 
 (define (one input)
-  (cons (sum-dups input 1) (sum-dups input (quotient (string-length input) 2))))
+  (cons (sum-dups input 1)
+        (sum-dups input (quotient (string-length input) 2))))
