a465d0c5a085ac706b8a1cf8168d4ca392832759
Satyen Rai <satyenr@users.noreply.github.com>
2019-12-05 15:38:48 +0530

    Read input as a F# Seq, not List
    The calling function can convert to List if needed. Additionally, use LF
    instead of CRLF.

--- src/AOC.fs
+++ src/AOC.fs
@@ -1,18 +1,18 @@
-﻿open System
+open System
 open System.IO
 
-(* 
-    The Tyranny of the Rocket Equation 
+(*
+    The Tyranny of the Rocket Equation
     https://adventofcode.com/2019/day/1
 *)
 let getMasses input =
-    input |> List.map int
+    input |> Seq.map int
 
 let fuel mass =
     max ((mass / 3) - 2) 0
 
 let totalFuel masses =
-    List.sumBy fuel masses
+    Seq.sumBy fuel masses
 
 let fuel' mass =
     let rec fuel'' acc m =
@@ -21,13 +21,13 @@ let fuel' mass =
             | 0 -> acc
             | _ -> fuel'' (acc + fm) (fm)
     fuel'' 0 mass
-        
+
 let totalFuel' masses =
-    List.sumBy fuel' masses
+    Seq.sumBy fuel' masses
 
 [<EntryPoint>]
 let main argv =
-    let input = File.ReadLines(argv.[0]) |> Seq.toList
-    let masses = getMasses input 
+    let input = File.ReadLines(argv.[0])
+    let masses = getMasses input
     printfn "The Tyranny of the Rocket Equation \t %d \t %d" (totalFuel masses) (totalFuel' masses)
     0
