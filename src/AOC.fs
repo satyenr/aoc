00fdd5e6e69a3ac3bcb439a50b6a782b92e29ba2
Satyen Rai <satyenr@users.noreply.github.com>
2019-12-05 15:23:54 +0530

    The Tyranny of the Rocket Equation (F#)
    Year: 2019
    Day: 1
    Language: F#

---
+++ src/AOC.fs
@@ -0,0 +1,33 @@
+﻿open System
+open System.IO
+
+(* 
+    The Tyranny of the Rocket Equation 
+    https://adventofcode.com/2019/day/1
+*)
+let getMasses input =
+    input |> List.map int
+
+let fuel mass =
+    max ((mass / 3) - 2) 0
+
+let totalFuel masses =
+    List.sumBy fuel masses
+
+let fuel' mass =
+    let rec fuel'' acc m =
+        let fm = fuel m
+        match m with
+            | 0 -> acc
+            | _ -> fuel'' (acc + fm) (fm)
+    fuel'' 0 mass
+        
+let totalFuel' masses =
+    List.sumBy fuel' masses
+
+[<EntryPoint>]
+let main argv =
+    let input = File.ReadLines(argv.[0]) |> Seq.toList
+    let masses = getMasses input 
+    printfn "The Tyranny of the Rocket Equation \t %d \t %d" (totalFuel masses) (totalFuel' masses)
+    0
