4188f6bcd93e0e093de5cfe4390b3cf163de4a70
Satyen Rai <satyenr@users.noreply.github.com>
2019-12-06 22:05:00 +0530

    1202 Program Alarm I (F#)
    Year: 2019
    Day: 2
    Language: F#

--- src/AOC.fs
+++ src/AOC.fs
@@ -1,10 +1,7 @@
-open System
+﻿open System
 open System.IO
 
-(*
-    The Tyranny of the Rocket Equation
-    https://adventofcode.com/2019/day/1
-*)
+(* The Tyranny of the Rocket Equation -- https://adventofcode.com/2019/day/1 *)
 let getMasses input =
     input |> Seq.map int
 
@@ -25,9 +22,47 @@ let fuel' mass =
 let totalFuel' masses =
     Seq.sumBy fuel' masses
 
+ (* 1202 Program Alarm -- https://adventofcode.com/2019/day/2 *)
+let getIntcodes (input: seq<string>) =
+    (input |> Seq.head).Split "," |> Array.map int
+
+let fixIntcodes (codes: int[]) =
+    codes.[1] <- 12
+    codes.[2] <- 2
+
+let runIntcodes (codes: int[]) =
+    let rec run pc =
+        let opcode = codes.[pc]
+        let fstOp = codes.[codes.[pc + 1]]
+        let sndOp = codes.[codes.[pc + 2]]
+        let resPos = codes.[pc + 3]
+        let nextPC = pc + 4
+        match opcode with
+            | 99 -> Some codes.[0]
+            | 1 -> codes.[resPos] <- fstOp + sndOp; run nextPC
+            | 2 -> codes.[resPos] <- fstOp * sndOp; run nextPC
+            | _ -> None
+    run 0
+
+let inputFile year day =
+    let prefix = "inputs"
+    [prefix; year; day] |> String.concat "/"
+
+let readInput prefix year day =
+    File.ReadLines([prefix; year; day;] |> String.concat "/")
+
 [<EntryPoint>]
 let main argv =
-    let input = File.ReadLines(argv.[0])
+    (* Day 1 *)
+    let input = File.ReadLines(inputFile "2019" "01.txt")
     let masses = getMasses input
-    printfn "The Tyranny of the Rocket Equation \t %d \t %d" (totalFuel masses) (totalFuel' masses)
+    printfn "%-50s \t %d \t %d" "The Tyranny of the Rocket Equation" (totalFuel masses) (totalFuel' masses)
+
+    (* Day 2 *)
+    let input = File.ReadLines(inputFile "2019" "02.txt")
+    let intcodes = getIntcodes input
+    fixIntcodes intcodes
+    match runIntcodes intcodes with
+        | Some n -> printfn "%-50s \t %d" "1202 Program Alarm" n
+        | None -> printfn "%-50s \t %s" "1202 Program Alarm" "Error"
     0
